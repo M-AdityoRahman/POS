8b57c154e70cd081d9cd6ccf96400d79370c56d7
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-07 00:27:19 +0700

    PWL_POS

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Kami</h1>
+    <a href="/products">Lihat Produk</a>
+    <br><br>
+    <a href="/user">Profile</a>
+    <br><br>
+    <a href="/sales">Transaksi</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+use Illuminate\Support\Facades\Route;
+
+// Halaman Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Halaman Products (Dengan Prefix)
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Halaman User dengan Parameter ID & Name
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// Halaman Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index']);
