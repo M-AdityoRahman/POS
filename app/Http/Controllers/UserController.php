78b2a372ee379a5319e78e916e3396e053a0ec86
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-15 03:12:54 +0700

    Jobsheet3_Praktikum6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function profile($id, $name)
     {
         return view('user', ['id' => $id, 'name' => $name]);
+    }
+
+    public function index()
+    {
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        
+        UserModel::where('username', 'customer-1')->update($data);
+
+
+        $users = UserModel::all();
+        return view('user', ['users' => $users]);
     }
 }
