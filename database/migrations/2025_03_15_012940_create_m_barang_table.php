d1c6ecb38e4c49ad70f7c93e933293c0b14017b0
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-15 01:32:53 +0700

    Jobsheet3_Praktikum2.2

---
+++ database/migrations/2025_03_15_012940_create_m_barang_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->string('kode_barang', 20)->unique();
+            $table->string('nama_barang', 100);
+            $table->integer('stok');
+            $table->decimal('harga', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
