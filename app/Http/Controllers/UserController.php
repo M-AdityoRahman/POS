f9e899b161bb9eac5e9b7d44f4192a7f5138408c
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-16 00:54:17 +0700

    Jobsheet4-Percobaan2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,24 +16,19 @@ class UserController extends Controller
 
     public function index()
 {
-    $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-    ];
-
-    // Gunakan updateOrCreate agar tidak menambah data duplikat
-    UserModel::updateOrCreate(
-        ['username' => 'manager_tiga'], 
-        $data
+    $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
     );
+    $user->save();
 
-    // Hitung jumlah user dengan level_id = 2
-    $userCount = UserModel::where('level_id', '2')->count();
-
-    return view('user', ['data' => $userCount]); 
+    return view('user', ['data' => $user]);
 }
+
 
 
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -13,10 +13,10 @@
             <th>ID Level Pengguna</th>
         </tr>
         <tr>
-            <td>{{ $data }}</td> 
-            <td>{{ $data }}</td>
-            <td>{{ $data }}</td>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td> 
+            <td>{{ $data->username }}</td>
+            <td>{{ $data ->nama}}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
