48ad3be7880e40a8e10252fb3fa8337b050313c0
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-15 23:51:10 +0700

    Jobsheet4_Praktikum2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,13 +22,13 @@ class UserController extends Controller
         'nama' => 'Manager 3',
         'password' => Hash::make('12345')
     ];
-
+    UserModel::where('username', 'manager_tiga')->delete();
     UserModel::create($data);
 
-    $user = UserModel::findOr(20,['username', 'nama'], function(){
-        about(404);
-    });
-    return view('user', ['data' => $user]);
+    $user = UserModel::where('username', 'manager9')->firstOrFail();
+
+    return view('user', ['data' => $user]); 
 }
 
 }
+
