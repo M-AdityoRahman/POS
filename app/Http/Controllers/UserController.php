1096d2ee321b16d12382b75abd77eb961e3885f6
M-AdityoRahman <adityorahman18@gmail.com>
2025-03-18 22:32:09 +0700

    Jobsheet5_Praktikum4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,9 @@ class UserController extends Controller
 
         $activeMenu = 'user'; //set Menu saat sedang active
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -37,6 +39,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
