adf33c9b69e7c94b0dee9daf6fc0cb6d7a1de6c9
M-AdityoRahman <adityorahman18@gmail.com>
2025-05-17 15:00:57 +0700

    Jobsheet8_Praktikum3

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
         Route::get('/export_excel', [BarangController::class, 'export_excel']); // ajax export excel
+        Route::get('/export_pdf', [BarangController::class, 'export_pdf']); // ajax pdf
     });
 
     Route::middleware(['authorize:ADM'])->group(function () {
